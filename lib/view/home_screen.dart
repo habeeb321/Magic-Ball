af7ddfc82a47736c53cf3d3e487f8ab2228599cb
habeebu <habeebrh321@gmail.com>
2023-08-02 20:18:41 +0530

    getx added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/instance_manager.dart';
+import 'package:magic_ball/controller/ball_controller.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends GetView<BallController> {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(() => BallController());
     return Scaffold(
       appBar: AppBar(
         title: const Text(
